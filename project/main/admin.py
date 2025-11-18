e33b065df6877fba33e39b645afe945ad45cdf1b
follow <follow12587@gmail.com>
2025-11-18 15:08:21 +1000

    made user products editable

--- project/main/admin.py
+++ project/main/admin.py
@@ -251,8 +251,8 @@ class UserProductsAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         return super().get_queryset(request).select_related("product", "user")
 
-    def has_change_permission(self, request, obj=...):
-        return False
+    # def has_change_permission(self, request, obj=...):
+    #     return False
         # return super().has_change_permission(request, obj)
 
 
